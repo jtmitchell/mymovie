37a83a5bd738f08e31453e17df07781dcd10424c
James Mitchell <james.mitchell@maungawhau.net.nz>
2015-05-28 23:07:00 +1200

    Use herokuapp.com for allowed hosts on heroku

--- app/settings/heroku.py
+++ app/settings/heroku.py
@@ -8,6 +8,8 @@ from .base import *
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+ALLOWED_HOSTS = ['.herokuapp.com']
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
