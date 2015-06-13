ee5feee63a5ccd8fc038591cf751fe37f2b6cea7
James Mitchell <james.mitchell@maungawhau.net.nz>
2015-06-13 16:26:33 +1200

    Test for getting a JWT from a gplus access token [#95109410]

--- users/tests/test_api.py
+++ users/tests/test_api.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from calendar import timegm
 
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from mock import patch
 from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework_jwt import utils
 
 from .factories import UserFactory
-from django.contrib.auth import get_user_model
 
 
 class TokenTestCase(TestCase):
@@ -83,3 +84,76 @@ class TestUserApi(TokenTestCase):
         self.assertEqual(response.data['last_name'], user.last_name)
         self.assertEqual(response.data['full_name'], user.get_full_name())
         self.assertEqual(response.data['avatar'], user.profile.avatar_url)
+
+gplus_user_data = {
+    'gender': 'male',
+    'objectType': 'person',
+    'kind': 'plus#person',
+    'etag': '"etagsample"',
+    'aboutMe': 'Description of me.',
+    'name': {
+        'givenName': 'James',
+        'familyName': 'Mitchell'
+        },
+    'id': '12345678',
+    'isPlusUser': True,
+    'verified': False,
+    'cover': {
+        'layout': 'banner',
+        'coverPhoto':
+            {
+                'width': 940,
+                'height': 625,
+                'url': 'https://lh3.googleusercontent.com/shadows.jpg'
+            },
+        'coverInfo': {
+            'topImageOffset': 0,
+            'leftImageOffset': 0
+            }
+        },
+    'displayName': 'James Mitchell',
+    'image': {
+        'isDefault': False,
+        'url': 'https://lh4.googleusercontent.com/photo.jpg?sz=50'
+        },
+    'language': 'en_GB',
+    'organizations': [
+        {
+            'primary': True,
+            'title': 'Web developer',
+            'type': 'work'
+        }
+    ],
+    'url': 'https://plus.google.com/+JamesMitchell',
+    'verified': False,
+    }
+
+
+class TestSocialAuth(TestCase):
+    """
+    Test that the user is registered if we get a social access token.
+    """
+    backend = 'google-plus'
+    access_token = 'sample.token'
+
+    def setUp(self):
+        self.client = APIClient(enforce_csrf_checks=True)
+
+    @patch('social.backends.base.BaseAuth.get_json')
+    def test_gplus_auth(self, mock):
+        """Exchange access token for JWT"""
+        mock.return_value = gplus_user_data
+
+        response = self.client.get(
+            '/api/v1/auth/register-by-token/{}'.format(self.backend),
+            params=dict(
+                access_token=self.access_token
+                ),
+            format='json',
+            )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         'Got error: {}'.format(response.content))
+
+        self.assertIn('token', response.data, 'Missing JWT')
+        self.assertIn('user', response.data, 'Missing User')
